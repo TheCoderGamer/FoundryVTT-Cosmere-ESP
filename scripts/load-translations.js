eff8e759d2bf2542c0617cea82ee5a23dbd69d39
Ignacio <45735795+TheCoderGamer@users.noreply.github.com>
2026-03-10 12:57:56 +0100

    Delete scripts directory

--- scripts/load-translations.js
+++
@@ -1,9 +0,0 @@
-Hooks.on("i18nInit", async () => {
-    if (game.i18n.lang !== "es") return;
-    
-    const response = await fetch("modules/cosmere-rpg-es/lang/es.json");
-    const data = await response.json();
-    foundry.utils.mergeObject(game.i18n.translations, data);
-    
-    console.log("Cosmere RPG | Traducciones en español cargadas.");
-});
